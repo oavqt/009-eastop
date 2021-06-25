e74f829c377378aa59addc7d6ee34cb1a5e8c5a5
oav <ortizqtv@gmail.com>
2021-06-24 21:17:25 -0500

    opacity option - minor tweaks to code

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,6 @@
 //ets
 //generate grid
+const container__grid = document.querySelector('.container__grid');
 const container__grid__one = document.querySelector('.container__grid__one');
 const container__grid__two = document.querySelector('.container__grid__two');
 const slider = document.querySelector('.container__right__slider');
@@ -56,24 +57,41 @@ function colorGrid() {
 
   let mouse = false;
 
-  grid__divs.forEach((square) => {
-    square.addEventListener('mousedown', (e) => {
+  grid__divs.forEach((div) => {
+    div.addEventListener('mousedown', (e) => {
       mouse = true;
-      square.setAttribute('style', `background-color: ${color}`);
       e.preventDefault();
-    });
-  });
-
-  grid__divs.forEach((square) => {
-    square.addEventListener('mouseover', (e) => {
-      if (mouse === true) {
-        square.setAttribute('style', `background-color: ${color}`);
+      div.style.backgroundColor = `${color}`;
+      if (opacity.checked) {
+        if (div.style.opacity >= 1) {
+          div.style.opacity = 0;
+        } else {
+          div.style.opacity = (+div.style.opacity + 0.1).toFixed(1);
+        }
+      } else {
+        div.style.opacity = 1;
       }
     });
   });
 
-  grid__divs.forEach((square) => {
-    square.addEventListener('mouseup', () => {
+  grid__divs.forEach((div) => {
+    div.addEventListener('mouseover', (e) => {
+      if (mouse === true) {
+        div.style.backgroundColor = `${color}`;
+        if (opacity.checked) {
+          if (div.style.opacity < 1) {
+            div.style.opacity = (+div.style.opacity + 0.1).toFixed(1);
+            console.log(div.style.opacity);
+          }
+        } else {
+          div.style.opacity = 1;
+        }
+      }
+    });
+  });
+
+  grid__divs.forEach((div) => {
+    div.addEventListener('mouseup', () => {
       if (mouse === true) {
         mouse = false;
       }
@@ -86,17 +104,45 @@ colorGrid();
 slider.addEventListener('input', (e) => {
   e.preventDefault();
   updateGrid();
+  updateSliderCount();
 });
 
 function updateGrid() {
-  container__grid__one.setAttribute(
-    'style',
-    `grid-template: repeat(${gridDimensions}, 1fr) / repeat(${gridDimensions}, 1fr)`
-  );
-  container__grid__two.setAttribute(
-    'style',
-    `grid-template: repeat(${gridDimensions}, 1fr) / repeat(${gridDimensions}, 1fr)`
-  );
+  container__grid__one.style.gridTemplate = `repeat(${gridDimensions}, 1fr) / repeat(${gridDimensions}, 1fr)`;
+  container__grid__two.style.gridTemplate = `repeat(${gridDimensions}, 1fr) / repeat(${gridDimensions}, 1fr)`;
 }
 
+const slider__number = document.querySelector(
+  '.container__right__slider__number'
+);
 
+function updateSliderCount() {
+  slider__number.textContent = String(gridDimensions);
+}
+
+const reset = document.querySelector('.container__right__reset');
+
+reset.addEventListener('click', resetGrid);
+
+function resetGrid() {
+  let = container__grid__divs = document.querySelectorAll(
+    '.container__grid__divs'
+  );
+
+  let grid__divs = [...container__grid__divs];
+
+  grid__divs.forEach((div) => {
+    div.setAttribute('style', 'background-color: white');
+  });
+}
+
+const changeBackground = document.querySelector(
+  '.container__right__color__background'
+);
+
+changeBackground.addEventListener('change', (e) => {
+  container__grid__one.style.backgroundColor = `${e.target.value}`;
+  container__grid__two.style.backgroundColor = `${e.target.value}`;
+});
+
+const opacity = document.querySelector('.container__right__opacity');
